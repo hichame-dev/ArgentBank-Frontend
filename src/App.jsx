379a275d983d5718fb192a07c654a950e3697816
hichame <shemsine@gmail.com>
2025-06-25 13:44:42 +0200

    feat(auth): add user state restoration on app initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
-import { loginSuccess } from './redux/slices/authSlice'
+import { loginSuccess, setUser } from './redux/slices/authSlice'
 
 import Layout from './components/layout/Layout'
 import Home from './pages/Home/Home'
@@ -11,15 +11,17 @@ import NotFound from './pages/NotFound/NotFound'
 import PrivateRoute from './components/PrivateRoute/PrivateRoute'
 import TransactionsPage from './pages/TransactionsPage/TransactionsPage'
 
-
-
 function App() {
   const dispatch = useDispatch()
 
   useEffect(() => {
     const token = localStorage.getItem('token')
-    if (token) {
+    const user = localStorage.getItem('user')
+
+    // ✅ On restaure uniquement si les deux sont présents
+    if (token && user) {
       dispatch(loginSuccess({ token }))
+      dispatch(setUser(JSON.parse(user)))
     }
   }, [dispatch])
 
@@ -29,12 +31,15 @@ function App() {
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={
-            <PrivateRoute><Profile /></PrivateRoute>} />
-          <Route path="/transactions" element={
-            <PrivateRoute><TransactionsPage /></PrivateRoute>} />
+          <Route
+            path="/profile"
+            element={<PrivateRoute><Profile /></PrivateRoute>}
+          />
+          <Route
+            path="/transactions"
+            element={<PrivateRoute><TransactionsPage /></PrivateRoute>}
+          />
         </Route>
-
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
